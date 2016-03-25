3c4dd61c15478792d443ce3993e03fc642e5f708
Kai Sellgren <kaisellgren@gmail.com>
2016-03-25 13:43:30 -0700

    Fix Dave's bad indentation

--- src/client/app/view.js
+++ src/client/app/view.js
@@ -10,23 +10,23 @@ class AppView extends Component {
 
     // Render some virtual dom stuff. Use our predefined helpers from the top of the file.
     return div({className: 'main'},
-        header({}),
-        ul({},
-          // the `data` is passed to MyApp component at the bottom of the file (in ReactDOM.render())
-          // and now we just 'loop' through the data, in this case, we map the todo items to todo elements.
-          mapObjectToList(view.todos, (todo, todoId) =>
-            li({key: todoId},
-              span({}, todo.message),
-              a({ onClick: inputSources.removeTodo.fromConstant(todoId) }, 'Remove')
-            )
+      header({}),
+      ul({},
+        // the `data` is passed to MyApp component at the bottom of the file (in ReactDOM.render())
+        // and now we just 'loop' through the data, in this case, we map the todo items to todo elements.
+        mapObjectToList(view.todos, (todo, todoId) =>
+          li({key: todoId},
+            span({}, todo.message),
+            a({ onClick: inputSources.removeTodo.fromConstant(todoId) }, 'Remove')
           )
-        ),
-        input({
-          value: view.newTodoMessage,
-          onChange: inputSources.newTodoMessage.fromValue, // Wire up onChange to add the input's value in newTodoMessage input source.
-          onKeyUp: inputSources.addNewTodo.fromOnlyKeyCode(13)
-        }),
-        button({ onClick: inputSources.addNewTodo.fromEvent }, 'Add!') // Wire up the input source. We don't care about a 'value', just that it happened.
+        )
+      ),
+      input({
+        value: view.newTodoMessage,
+        onChange: inputSources.newTodoMessage.fromValue, // Wire up onChange to add the input's value in newTodoMessage input source.
+        onKeyUp: inputSources.addNewTodo.fromOnlyKeyCode(13)
+      }),
+      button({ onClick: inputSources.addNewTodo.fromEvent }, 'Add!') // Wire up the input source. We don't care about a 'value', just that it happened.
     )
   }
 }
